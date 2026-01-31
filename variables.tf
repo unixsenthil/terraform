e25e9ef2892c2cd813534504c9630a1c64ad5926
vagrant <vagrant@sloopstash-ubuntu-linux-24-04-server>
2026-01-31 13:38:58 +0000

    Add multi-cloud Azure, AWS - Terraform configuration

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "environment" {
+  type = string
+  description = "Environment."
+}
+variable "azure_subscription_id" {
+  type = string
+  description = "Azure subscription identifier."
+}
